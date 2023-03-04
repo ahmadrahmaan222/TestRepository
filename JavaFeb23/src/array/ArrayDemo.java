9d9a63b992ae70ee89eed501f9921f0b5e483b34
ahmadrahmaan222 <126289895+ahmadrahmaan222@users.noreply.github.com>
2023-03-04 12:59:56 -0500

    Update ArrayDemo.java
    //Additional optional comments

--- JavaFeb23/src/array/ArrayDemo.java
+++ JavaFeb23/src/array/ArrayDemo.java
@@ -3,7 +3,7 @@ package array;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		
+		// this is an one dimensional array
 		///declaring array
 		String[] myArray = new String[3];
 		//assigning value to the array
